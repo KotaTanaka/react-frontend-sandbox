9fe589590c3e3cc1e8fde244b4a27db7c63951c1
Kota Tanaka <te11k.tanaka@gmail.com>
2019-10-09 12:35:04 +0900

    feat: APIパス定数化モジュール作成

---
+++ src/constants/Api.ts
@@ -0,0 +1,10 @@
+// TODO 設定ファイルに書く
+const SERVER_HOST = "http://localhost:1323";
+
+/**
+ * APIエンドポイント
+ */
+export const API_ENDPOINT = {
+  // Wi-Fiサービス一覧取得
+  SERVICES: SERVER_HOST + "/admin/services"
+}

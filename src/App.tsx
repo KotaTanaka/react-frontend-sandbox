f6e643994d7316b62c5d8d6b61a93b6b33eb495f
Kota Tanaka <te11k.tanaka@gmail.com>
2019-10-09 16:53:32 +0900

    feat: Providerの設置

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 // from app
+import { Provider } from "Store";
 import Navigation from 'components/Navigation';
 import TopPage from 'pages/TopPage';
 import ServicesPage from 'pages/ServicesPage';
@@ -9,25 +10,27 @@ import ServiceDetailPage from 'pages/ServiceDetailPage';
 
 const App: React.FC = () => {
   return (
-    <Router>
-      <div>
-        <Navigation />
-        <Switch>
-          {/* Wi-Fiサービス詳細 */}
-          <Route path="/services/:serviceId">
-            <ServiceDetailPage />
-          </Route>
-          {/* Wi-Fiサービス一覧 */}
-          <Route path="/services">
-            <ServicesPage />
-          </Route>
-          {/* トップページ */}
-          <Route path="/">
-            <TopPage />
-          </Route>
-        </Switch>
-      </div>
-    </Router>
+    <Provider>
+      <Router>
+        <div>
+          <Navigation />
+          <Switch>
+            {/* Wi-Fiサービス詳細 */}
+            <Route path="/services/:serviceId">
+              <ServiceDetailPage />
+            </Route>
+            {/* Wi-Fiサービス一覧 */}
+            <Route path="/services">
+              <ServicesPage />
+            </Route>
+            {/* トップページ */}
+            <Route path="/">
+                <TopPage />
+            </Route>
+          </Switch>
+        </div>
+      </Router>
+    </Provider>
   );
 }
 

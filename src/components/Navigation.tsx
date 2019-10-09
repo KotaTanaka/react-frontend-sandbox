84b257873e6659c36be4862d8c837a5b1f165ffb
Kota Tanaka <te11k.tanaka@gmail.com>
2019-10-09 12:35:26 +0900

    feat: 画面が移動するたびに行う処理を追加

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,11 +1,22 @@
-import React from 'react';
-import { Link } from "react-router-dom";
+import React, { useEffect } from 'react';
+import { Link, useLocation } from "react-router-dom";
 
 /**
  * ナビゲーションバー
  * @author kotatanaka
  */
 const Navigation: React.FC = () => {
+  let location = useLocation()
+
+  /** 画面が移動するたびに行う処理 */
+  useEffect(() => {
+    const { pathname, search } = location;
+    console.log(pathname);
+    if (search) {
+      console.log(search)
+    }
+  }, [location]);
+
   /** リンク */
   const link = (path: string, label: string) => (
     <li>

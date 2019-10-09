4bac68c5fb70a672eb7e708b82d147aa3821a239
Kota Tanaka <te11k.tanaka@gmail.com>
2019-10-09 16:52:21 +0900

    feat: Reducerの作成

---
+++ src/Reducer.ts
@@ -0,0 +1,32 @@
+/** Global State */
+export interface State {
+  count: number;
+}
+
+/** アクション種別 */
+export enum ActionType {
+  SET_COUNT = "SET_COUNT",
+}
+
+/** アクション */
+export interface Action {
+  type: ActionType;
+  payload: any;
+}
+
+/** Reducer */
+const Reducer = (state: State, action: Action) => {
+  const { type, payload } = action;
+
+  switch (type) {
+    case ActionType.SET_COUNT:
+      return {
+        ...state,
+        count: payload
+      };
+    default:
+      return state;
+  }
+};
+
+export default Reducer;

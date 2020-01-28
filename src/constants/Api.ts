e74995fb18e5542c8d912b9d5a760d84aaf1d18a
KotaTanaka <te11k.tanaka@gmail.com>
2020-01-28 12:31:00 +0900

    refactor: APIホストをenvから参照

--- src/constants/Api.ts
+++ src/constants/Api.ts
@@ -1,10 +1,7 @@
-// TODO 設定ファイルに書く
-const SERVER_HOST = 'http://localhost:1323';
+const HOST = process.env.REACT_APP_API_HOST;
 
-/**
- * APIエンドポイント
- */
+/** APIエンドポイント */
 export const API_ENDPOINT = {
   // Wi-Fiサービス一覧取得
-  SERVICES: SERVER_HOST + '/admin/services'
+  SERVICES: `${HOST}/admin/services`
 };

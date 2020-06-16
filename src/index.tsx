eb47d8abbad60c9a5b5af30beed0385063b37f26
KotaTanaka <te11k.tanaka@gmail.com>
2020-06-16 16:28:18 +0900

    remove: ServiceWorker

--- src/index.tsx
+++ src/index.tsx
@@ -2,8 +2,5 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import 'src/index.css';
 import App from 'src/App';
-import * as serviceWorker from 'src/serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
-
-serviceWorker.unregister();

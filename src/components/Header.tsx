5b2df3396afa2f966d4a610ec3b3a3210eba470c
KotaTanaka <te11k.tanaka@gmail.com>
2020-06-16 15:15:13 +0900

    feat: サイドバー導入に伴うヘッダ調整

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
-import { AppBar, Toolbar, Typography, Button } from '@material-ui/core';
+import { AppBar, Toolbar, Typography } from '@material-ui/core';
 import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
 
 // from app
@@ -9,19 +9,15 @@ import usePageTransition from 'src/hooks/usePageTransition';
 /** 固定ヘッダー */
 const Header: React.FC = () => {
   const classes = useStyles();
-  const { moveToHome, moveToServices } = usePageTransition();
+
+  const { moveToHome } = usePageTransition();
 
   return (
-    <AppBar position="static">
-      <Toolbar className={classes.toolbar}>
-        <BarLeft>
-          <Title onClick={moveToHome}>
-            <Typography variant="h6">Find Wi-Fi</Typography>
-          </Title>
-        </BarLeft>
-        <Button color="inherit" onClick={moveToServices}>
-          Wi-Fiサービス一覧
-        </Button>
+    <AppBar position="fixed" className={classes.appBar}>
+      <Toolbar>
+        <Title onClick={moveToHome}>
+          <Typography variant="h6">Find Wi-Fi</Typography>
+        </Title>
       </Toolbar>
     </AppBar>
   );
@@ -30,14 +26,11 @@ const Header: React.FC = () => {
 // styles
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
-    toolbar: {
-      flexGrow: 1,
+    appBar: {
+      zIndex: theme.zIndex.drawer + 1,
     },
   }),
 );
-const BarLeft = styled.div`
-  flex-grow: 1;
-`;
 const Title = styled.div`
   cursor: pointer;
 `;

6b4ee9ae82c3a86a5319e58da020f0fe03e43250
KotaTanaka <te11k.tanaka@gmail.com>
2020-06-16 15:15:36 +0900

    feat: ボディの位置調整

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import styled from 'styled-components';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 // from app
 import { Provider } from 'src/Store';
 import Header from 'src/components/Header';
+import SideBar from 'src/components/SideBar';
 import TopPage from 'src/pages/TopPage';
 import ServicesPage from 'src/pages/ServicesPage';
 import ServiceDetailPage from 'src/pages/ServiceDetailPage';
@@ -13,19 +15,28 @@ const App: React.FC = () => {
   return (
     <Provider>
       <Router>
-        <div>
+        <AppContainer>
           <Header />
-          <Switch>
-            <Route exact path="/" component={TopPage} />
-            <Route exact path="/services" component={ServicesPage} />
-            {/* prettier-ignore */}
-            <Route exact path="/services/:serviceId" component={ServiceDetailPage} />
-            <Route exact path="/counter" component={CounterPage} />
-          </Switch>
-        </div>
+          <SideBar />
+          <Body>
+            <Switch>
+              <Route exact path="/" component={TopPage} />
+              <Route exact path="/services" component={ServicesPage} />
+              {/* prettier-ignore */}
+              <Route exact path="/services/detail/:serviceId" component={ServiceDetailPage} />
+              <Route exact path="/counter" component={CounterPage} />
+            </Switch>
+          </Body>
+        </AppContainer>
       </Router>
     </Provider>
   );
 };
+
+// styles
+const AppContainer = styled.div``;
+const Body = styled.div`
+  margin: 64px 0 0 240px;
+`;
 
 export default App;

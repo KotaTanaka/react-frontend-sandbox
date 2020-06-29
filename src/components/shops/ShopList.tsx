bd985ea3b09059df2b415a911636f888b8646107
KotaTanaka <te11k.tanaka@gmail.com>
2020-06-29 20:39:47 +0900

    feat: 店舗一覧の表示

---
+++ src/components/shops/ShopList.tsx
@@ -0,0 +1,88 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import styled from 'styled-components';
+import {
+  Button,
+  Paper,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+} from '@material-ui/core';
+
+// from app
+import usePageTransition from 'src/hooks/usePageTransition';
+import { IShopList } from 'src/interfaces/api/response/Shop';
+
+interface Props {
+  data: IShopList;
+  loading: boolean;
+}
+
+/** 店舗リスト */
+const ShopList: React.FC<Props> = (props: Props) => {
+  const { data, loading } = props;
+  const classes = useStyles();
+
+  const { moveToShopDetail } = usePageTransition();
+
+  if (loading) {
+    return <p>Loading...</p>;
+  }
+
+  return (
+    <Container>
+      <TableContainer component={Paper}>
+        <Table className={classes.table}>
+          <TableHead>
+            <TableRow>
+              <TableCell>店舗名</TableCell>
+              <TableCell>サービス名</TableCell>
+              <TableCell>エリア</TableCell>
+              <TableCell>住所</TableCell>
+              <TableCell>レビュー数</TableCell>
+              <TableCell>評価</TableCell>
+              <TableCell />
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {data.shopList.map((shop) => (
+              <TableRow key={shop.shopId}>
+                <TableCell>{shop.shopName}</TableCell>
+                <TableCell>{shop.wifiName}</TableCell>
+                <TableCell>{shop.area}</TableCell>
+                <TableCell>{shop.address}</TableCell>
+                <TableCell>{shop.reviewCount}</TableCell>
+                <TableCell>{shop.average}</TableCell>
+                <TableCell>
+                  <Button
+                    color="primary"
+                    className={classes.button}
+                    onClick={() => moveToShopDetail(shop.shopId)}
+                  >
+                    詳細
+                  </Button>
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </Container>
+  );
+};
+
+// styles
+const useStyles = makeStyles({
+  table: {
+    minWidth: 640,
+  },
+  button: {
+    padding: 0,
+  },
+});
+const Container = styled.div``;
+
+export default ShopList;
--- src/pages/ShopsPage.tsx
+++ src/pages/ShopsPage.tsx
@@ -3,14 +3,19 @@ import styled from 'styled-components';
 
 // from app
 import { PAGES } from 'src/constants/page';
+import useGetShops from 'src/hooks/useGetShops';
 import PageHeading from 'src/components/partials/PageHeading';
+import ShopList from 'src/components/shops/ShopList';
 import { flexColumnCenter } from 'src/styles/mixin';
 
 /** 店舗一覧ページ */
 const ShopsPage: React.FC = () => {
+  const { shops, isShopsLoading } = useGetShops();
+
   return (
     <Container>
       <PageHeading heading={PAGES.SHOPS.name} />
+      <ShopList data={shops} loading={isShopsLoading} />
     </Container>
   );
 };

34554e929fce9dd0faf8b6ef019ef4795ad8226e
KotaTanaka <te11k.tanaka@gmail.com>
2020-07-17 15:43:48 +0900

    feat: 入力フォームコンポーネント

---
+++ src/components/partials/FormInput.tsx
@@ -0,0 +1,44 @@
+import React, { useCallback } from 'react';
+import { TextField } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+
+interface Props {
+  label: string;
+  help: string;
+  type?: string;
+  value: string | number;
+  onChange: (value: any) => void;
+}
+
+/** 入力フォーム */
+const FormInput: React.FC<Props> = (props: Props) => {
+  const { label, help, type, value, onChange } = props;
+  const classes = useStyles();
+
+  // prettier-ignore
+  const handleChange = useCallback((e) => {
+    onChange(e.target.value);
+  }, [onChange]);
+
+  return (
+    <TextField
+      label={label}
+      helperText={help}
+      variant="outlined"
+      type={type}
+      className={classes.textField}
+      value={value}
+      onChange={handleChange}
+    />
+  );
+};
+
+// styles
+const useStyles = makeStyles({
+  textField: {
+    width: 320,
+    marginBottom: 16,
+  },
+});
+
+export default FormInput;

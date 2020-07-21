cc42b5bff9f868c9258be22d4a1160632e4de1aa
KotaTanaka <te11k.tanaka@gmail.com>
2020-07-21 14:32:50 +0900

    feat: プライマリーボタンコンポーネント

---
+++ src/components/partials/ButtonPrimary.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import { Button } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+
+interface Props {
+  label: string;
+  icon: React.ReactNode;
+  onClick: () => void;
+}
+
+/** プライマリーボタン */
+const ButtonPrimary: React.FC<Props> = (props: Props) => {
+  const { label, icon, onClick } = props;
+  const classes = useStyles();
+
+  return (
+    <Button
+      variant="contained"
+      color="primary"
+      size="large"
+      startIcon={icon}
+      className={classes.button}
+      onClick={onClick}
+    >
+      {label}
+    </Button>
+  );
+};
+
+// styles
+const useStyles = makeStyles({
+  button: {
+    width: 240,
+  },
+});
+
+export default ButtonPrimary;

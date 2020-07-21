948a7c26afc3471f534f5d9604227061d56a077e
KotaTanaka <te11k.tanaka@gmail.com>
2020-07-21 19:52:58 +0900

    refactor: TextFieldはFormControllでのラップ不要

--- src/components/areas/AreaRegisterForm.tsx
+++ src/components/areas/AreaRegisterForm.tsx
@@ -1,13 +1,12 @@
 import React from 'react';
 import styled from 'styled-components';
-import { FormControl } from '@material-ui/core';
-import { makeStyles } from '@material-ui/core/styles';
 import { Save } from '@material-ui/icons';
 
 // from app
 import FormInput from 'src/components/partials/Form/FormInput';
 import ButtonPrimary from 'src/components/partials/Button/ButtonPrimary';
 import { IRegisterAreaBody } from 'src/interfaces/api/request/Area';
+import { flexColumnCenter } from 'src/styles/mixin';
 
 interface Props {
   params: IRegisterAreaBody;
@@ -19,35 +18,29 @@ interface Props {
 /** エリア登録フォーム */
 const AreaRegisterForm: React.FC<Props> = (props: Props) => {
   const { params, onChangeAreaKey, onChangeAreaName, onSave } = props;
-  const classes = useStyles();
 
   return (
     <Container>
-      <FormControl className={classes.form}>
-        <FormInput
-          label="エリアキー"
-          help="エリアキーを入力"
-          value={params.areaKey}
-          onChange={onChangeAreaKey}
-        />
-        <FormInput
-          label="エリア名"
-          help="エリア名称を入力"
-          value={params.areaName}
-          onChange={onChangeAreaName}
-        />
-        <ButtonPrimary label="登録する" icon={<Save />} onClick={onSave} />
-      </FormControl>
+      <FormInput
+        label="エリアキー"
+        help="エリアキーを入力"
+        value={params.areaKey}
+        onChange={onChangeAreaKey}
+      />
+      <FormInput
+        label="エリア名"
+        help="エリア名称を入力"
+        value={params.areaName}
+        onChange={onChangeAreaName}
+      />
+      <ButtonPrimary label="登録する" icon={<Save />} onClick={onSave} />
     </Container>
   );
 };
 
 // styles
-const useStyles = makeStyles({
-  form: {
-    alignItems: 'center',
-  },
-});
-const Container = styled.div``;
+const Container = styled.div`
+  ${flexColumnCenter};
+`;
 
 export default AreaRegisterForm;
--- src/components/services/ServiceRegisterForm.tsx
+++ src/components/services/ServiceRegisterForm.tsx
@@ -1,13 +1,12 @@
 import React from 'react';
 import styled from 'styled-components';
-import { FormControl } from '@material-ui/core';
-import { makeStyles } from '@material-ui/core/styles';
 import { Save } from '@material-ui/icons';
 
 // from app
 import FormInput from 'src/components/partials/Form/FormInput';
 import ButtonPrimary from 'src/components/partials/Button/ButtonPrimary';
 import { IRegisterServiceBody } from 'src/interfaces/api/request/Service';
+import { flexColumnCenter } from 'src/styles/mixin';
 
 interface Props {
   params: IRegisterServiceBody;
@@ -19,35 +18,29 @@ interface Props {
 /** Wi-Fiサービス登録フォーム */
 const ServiceRegisterForm: React.FC<Props> = (props: Props) => {
   const { params, onChangeWifiName, onChangeLink, onSave } = props;
-  const classes = useStyles();
 
   return (
     <Container>
-      <FormControl className={classes.form}>
-        <FormInput
-          label="サービス名"
-          help="Wi-Fiのサービス名称を入力"
-          value={params.wifiName}
-          onChange={onChangeWifiName}
-        />
-        <FormInput
-          label="リンク"
-          help="公式サイトのURL等を入力"
-          value={params.link}
-          onChange={onChangeLink}
-        />
-        <ButtonPrimary label="登録する" icon={<Save />} onClick={onSave} />
-      </FormControl>
+      <FormInput
+        label="サービス名"
+        help="Wi-Fiのサービス名称を入力"
+        value={params.wifiName}
+        onChange={onChangeWifiName}
+      />
+      <FormInput
+        label="リンク"
+        help="公式サイトのURL等を入力"
+        value={params.link}
+        onChange={onChangeLink}
+      />
+      <ButtonPrimary label="登録する" icon={<Save />} onClick={onSave} />
     </Container>
   );
 };
 
 // styles
-const useStyles = makeStyles({
-  form: {
-    alignItems: 'center',
-  },
-});
-const Container = styled.div``;
+const Container = styled.div`
+  ${flexColumnCenter};
+`;
 
 export default ServiceRegisterForm;

c83c15cc26cec66381a7f9ca8643d7cbabc2cbbe
KotaTanaka <te11k.tanaka@gmail.com>
2020-08-04 12:11:00 +0900

    feat: 確認ダイアログコンポーネント

---
+++ src/components/partials/ConfirmDialog.tsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import {
+  Button,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogContentText,
+} from '@material-ui/core';
+
+interface Props {
+  isOpen: boolean;
+  message: string;
+  onSubmit: () => void;
+  onCancel: () => void;
+}
+
+/** 確認ダイアログ */
+const ConfirmDialog: React.FC<Props> = (props: Props) => {
+  const { isOpen, message, onSubmit, onCancel } = props;
+
+  return (
+    <Dialog open={isOpen} onClose={onCancel}>
+      <DialogContent>
+        <DialogContentText>{message}</DialogContentText>
+      </DialogContent>
+      <DialogActions>
+        <Button onClick={onSubmit} color="primary">
+          OK
+        </Button>
+        <Button onClick={onCancel} color="primary" autoFocus>
+          キャンセル
+        </Button>
+      </DialogActions>
+    </Dialog>
+  );
+};
+
+export default ConfirmDialog;

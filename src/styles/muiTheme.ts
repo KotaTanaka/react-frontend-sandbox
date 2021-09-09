59e79d984a9dac58a625888797f8e03641209b9f
KotaTanaka <te11k.tanaka@gmail.com>
2021-09-09 14:30:45 +0900

    update: createMuiTheme -> createTheme

--- src/styles/muiTheme.ts
+++ src/styles/muiTheme.ts
@@ -1,7 +1,7 @@
-import { createMuiTheme } from '@material-ui/core/styles';
+import { createTheme } from '@material-ui/core/styles';
 
 /** MaterialUIのデフォルトテーマ */
-export const theme = createMuiTheme({
+export const theme = createTheme({
   /** テーマカラー */
   palette: {
     primary: {

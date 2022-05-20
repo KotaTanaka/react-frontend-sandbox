b2c2b899a73a9b02cb93d6e080c53405301500e0
KotaTanaka <te11k.tanaka@gmail.com>
2022-05-20 22:27:16 +0900

    feat: ヘッダーのデザイン調整

--- src/components/partials/Header.tsx
+++ src/components/partials/Header.tsx
@@ -12,7 +12,12 @@ const Header: React.FC = () => {
     <AppBar position="fixed" className={classes.appBar}>
       <Toolbar>
         <Title onClick={moveToHome}>
-          <Typography variant="h6">Find Wi-Fi</Typography>
+          <Typography variant="h5" className={classes.titleText}>
+            Find Wi-Fi
+          </Typography>
+          <Typography className={classes.titleSubText}>
+            Admin Console
+          </Typography>
         </Title>
       </Toolbar>
     </AppBar>
@@ -24,11 +29,22 @@ const useStyles = makeStyles((theme: Theme) =>
   createStyles({
     appBar: {
       zIndex: theme.zIndex.drawer + 1,
+      background: `linear-gradient(to bottom, #3f51b5, #6495ed)`,
+    },
+    titleText: {
+      letterSpacing: 2,
+      textShadow: '1px 1px 2px #b0e0e6',
+    },
+    titleSubText: {
+      marginLeft: theme.spacing(1.5),
+      letterSpacing: 1,
     },
   }),
 );
 const Title = styled.div`
   cursor: pointer;
+  display: flex;
+  align-items: baseline;
 `;
 
 export default Header;
